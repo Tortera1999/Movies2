92097d1581022c48fedae6513ff818eb22d74db0
Tortera1999 <nikhilnarendra1999@gmail.com>
2018-07-22 17:39:05 +0530

    Add my files are there

---
+++ Movies2/MovieCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieCollectionViewCell.swift
+//  Movies2
+//
+//  Created by Nikhil Iyer on 7/22/18.
+//  Copyright © 2018 Nikhil Iyer. All rights reserved.
+//
+
+import UIKit
+
+class MovieCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var moviePosterPic: UIImageView!
+    
+}
